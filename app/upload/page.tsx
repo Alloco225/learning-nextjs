e862265ec93685fd40aaee025ac12f1fa58f1f21
Alloco + Ketchup <amanehosanna@gmail.com>
2024-06-17 23:53:17 +0000

    💥 NEW: file upload with cloudinary

---
+++ app/upload/page.tsx
@@ -0,0 +1,33 @@
+'use client';
+import React, { useState } from 'react'
+import {CldUploadWidget, CldImage} from 'next-cloudinary';
+
+interface CloudinaryResult {
+    public_id: string
+}
+
+const UploadPage = () => {
+    const [publicId, setPublicId] = useState('');
+  return (
+		<>
+        {publicId && <CldImage src={publicId} width={270} height={180} alt='Image'/>}
+			<CldUploadWidget uploadPreset="rp6qncz3"
+                onSuccess={(result, widget) => {
+                    console.log("onSuccess", result);
+                    const info = result.info as CloudinaryResult;
+                    setPublicId(info.public_id);
+                }}
+                onError={(error, widget) => {
+                    
+                    console.log("onSuccess", error);
+                }}
+            >
+				{({ open }) => {
+					return <button onClick={() => open()} className='btn btn-primary'>Upload</button>;
+				}}
+			</CldUploadWidget>
+		</>
+	);
+}
+
+export default UploadPage
