95227b4f8ef712b1d4361621a784e94840b02691
Alloco + Ketchup <amanehosanna@gmail.com>
2024-06-18 22:25:46 +0000

    🚀 RELEASE: build release

---
+++ app/api/send-email/sendEmail.ts
@@ -0,0 +1,16 @@
+// import WelcomeTemplate from "@/emails/WelcomeTemplate";
+// import { NextRequest, NextResponse } from "next/server";
+// import { Resend } from "resend"
+
+// const resend = new Resend(process.env.RESEND_API_KEY);
+
+// export default async function POST (request: NextRequest) {
+//     await resend.emails.send({
+//         from: '...',
+//         to: 'amane.dev@gmail.com',
+//         subject: 'Hello from Resend!',
+//         react: WelcomeTemplate({ name: 'Amane' }),
+//     });
+
+//     return NextResponse.json({ success: true });
+// }
