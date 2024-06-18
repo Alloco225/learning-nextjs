ce12e3e21a9473e1304172a2e97b29388c59afae
Alloco + Ketchup <amanehosanna@gmail.com>
2024-06-18 18:25:52 +0000

    💥 NEW: next auth && session

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,11 +1,19 @@
+'use client';
+import { useSession } from "next-auth/react";
 import Link from "next/link";
 import React from "react";
 
 const NavBar = () => {
+	const {status, data: session} = useSession();
+
+	
 	return <nav className="flex justify-between p-5 bg-slate-200">
 		<Link href="/">Nextjs app</Link>
-		<ul>
+		<ul className="flex gap-3">
 			<Link href="/users">Users</Link>
+			{ status === 'loading' && <div className="">Loading...</div>}
+			{ status === 'authenticated' && <div>{session.user!.name}</div>}
+			{ status === 'unauthenticated' && <Link href="/api/auth/signin">Login</Link>}
 		</ul>
 	</nav>;
 };
